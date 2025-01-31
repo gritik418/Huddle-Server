3c10ee5dc0e7c0be662633c51791d55fc52af495
Ritik <160255700+gritik418@users.noreply.github.com>
2025-01-31 23:49:24 +0530

    Chat model created

---
+++ src/models/Chat.ts
@@ -0,0 +1,38 @@
+import mongoose, { Model, Schema, Types } from "mongoose";
+
+const ChatSchema = new Schema<Chat>(
+  {
+    isGroupChat: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    groupName: {
+      type: String,
+    },
+    groupIcon: {
+      type: String,
+    },
+    admins: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    members: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lastMessage: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Chat: Model<Chat> =
+  mongoose.models.Chat || mongoose.model("Chat", ChatSchema);
+
+export default Chat;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,11 @@
 interface User {
   firstName: string;
-  lastName: string;
+  lastName?: string;
   username: string;
   email: string;
-  password: string;
-  profilePicture: string;
-  bio: string;
+  password?: string;
+  profilePicture?: string;
+  bio?: string;
   isVerified: boolean;
   isActive: boolean;
   provider: "credentials" | "google";
@@ -13,4 +13,17 @@ interface User {
   friendRequests: Types.ObjectId[];
   blockedUsers: Types.ObjectId[];
   posts: Types.ObjectId[];
+  verificationCode?: string;
+  verificationCodeExpiry?: Date;
+  passwordResetToken?: string;
+  passwordResetTokenExpiry?: Date;
+}
+
+interface Chat {
+  isGroupChat: boolean;
+  groupName?: string;
+  groupIcon?: string;
+  members: Types.ObjectId[];
+  admins?: Types.ObjectId[];
+  lastMessage?: string;
 }
